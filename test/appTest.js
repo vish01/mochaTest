155d8ab250feea7b7edf442968556004ee12ac35
Vishal Limbachiya <vishallimbachiya379@gmail.com>
2019-01-07 20:33:44 -0500

    included 2 functions

--- test/appTest.js
+++ test/appTest.js
@@ -1,15 +1,33 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-// const app = require('../app.js');
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
 
+const app = require('../app.js');
+
+//Results 
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 describe('App', function(){
-    it('sayHello should return hello', function(){
-        let result = sayHello();
-        assert.equal(result, 'hello');
+
+    describe('sayHello()', function(){
+        it('sayHello should return hello', function(){
+            assert.equal(sayHelloResult, 'hello');
+        });
+    
+        it('sayHello should return type string', function(){
+            assert.typeOf(sayHelloResult, 'string');
+        });
+    });
+    
+    describe('addNumbers()', function(){
+        it('addNumbers should be above 5', function(){
+            assert.isAbove(addNumbersResult, 5);
+        });
+    
+        it('addNumbers should return type number', function(){
+            assert.typeOf(addNumbersResult, 'number');
+        });
     });
 
-    it('sayHello should return type string', function(){
-        let result = sayHello();
-        assert.typeOf(result, 'string');
-    });
+   
 });
